c5af652e05eb2f0c778dc6db60df26ee536c79d0
patrickkwang <patrick@covar.com>
2019-07-22 22:09:55 -0400

    new and improved tests

--- tests/test_novelty.py
+++ tests/test_novelty.py
@@ -1,12 +1,13 @@
-"""Test correctness (publication) weighting."""
+"""Test novelty weighting."""
 # pylint: disable=redefined-outer-name,no-name-in-module,unused-import
 # ^^^ this stuff happens because of the incredible way we do pytest fixtures
 import json
-from messenger.modules.weight_correctness import query as weight
+from messenger.modules.weight_novelty import query as novelty
+from messenger.modules.weight_correctness import query as correctness
 from .fixtures import to_weight
 
 
 def test_weight(to_weight):
     """Test that weight() runs without errors."""
-    result = weight(to_weight)
+    result = correctness(novelty(to_weight))
     print(json.dumps(result, indent=4))
---
+++ tests/test_score.py
@@ -0,0 +1,12 @@
+"""Test scoring."""
+# pylint: disable=redefined-outer-name,no-name-in-module,unused-import
+# ^^^ this stuff happens because of the incredible way we do pytest fixtures
+import json
+from messenger.modules.score import query as score
+from .fixtures import weighted2
+
+
+def test_score(weighted2):
+    """Test that score() runs without errors."""
+    result = score(weighted2)
+    print(json.dumps(result, indent=4))
